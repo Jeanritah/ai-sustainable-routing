9d35126f7c60f5d9738af0b4ac6671492a2a8527
thorakatrinn <thorak23@ru.is>
2026-01-22 16:16:36 +0000

    load_grid.py

--- src/load_grid.py
+++ src/load_grid.py
@@ -1,36 +1,14 @@
 
-
 import geopandas as gpd
-import osmnx as ox
 
 uhei = gpd.read_file("grid_uhei.geojson")
-ndvi = gpd.read_file("grid_ndvi.geojson")
 
 print("UHEI columns:", uhei.columns)
-print("NDVI columns:", ndvi.columns)
-
-print("UHEI sample:")
-print(uhei.head())
-
-print("NDVI sample:")
-print(ndvi.head())
-
-
-
 print(uhei[["verde_pc", "area_grid"]].head())
+print("verde_pc min/max:", uhei["verde_pc"].min(), uhei["verde_pc"].max())
 
-# If verde_pc looks like 0–1:
-#   heat_score = 1 - verde_pc
-# If it looks like 0–100:
-#   heat_score = 1 - (verde_pc / 100)
+# Heat proxy: less green -> more heat
+uhei["heat_score"] = 1 - (uhei["verde_pc"] / 100.0)
 
-# Let's handle both cases robustly by normalizing
-verde_min = uhei["verde_pc"].min()
-verde_max = uhei["verde_pc"].max()
+print(uhei[["verde_pc", "heat_score"]].head())
 
-uhei["verde_pc_norm"] = (uhei["verde_pc"] - verde_min) / (verde_max - verde_min)
-
-# Heat proxy: less green → higher heat
-uhei["heat_score"] = 1 - uhei["verde_pc_norm"]
-
-print(uhei[["verde_pc", "verde_pc_norm", "heat_score"]].head())
